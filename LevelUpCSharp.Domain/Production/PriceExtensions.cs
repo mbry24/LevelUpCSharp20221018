4b504cb45b9286a772dda69e3aac67bc725f6b8f
Pior Barankiewicz <piotr.barankiewicz@gmail.com>
2022-10-18 10:39:44 +0200

    extension - solution

---
+++ LevelUpCSharp.Domain/Production/PriceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LevelUpCSharp.Production
+{
+	public static class PriceExtensions
+	{
+		public static decimal GetWithVat(this decimal source, int vat)
+		{
+			if (vat < 0 || vat > 23)
+			{
+				throw new ArgumentException("out of range");
+			}
+
+			return source * (1 + (decimal) vat / 100);
+		}
+
+		public static decimal GetVat(this decimal source, int vat)
+		{
+			if (vat < 0 || vat > 23)
+			{
+				throw new ArgumentException("out of range");
+			}
+
+			return source * (decimal)vat / 100;
+		}
+	}
+}
---
+++ LevelUpCSharp.Tools/DecimalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LevelUpCSharp
+{
+	public static class DecimalExtensions
+	{
+		public static decimal Multiplay(this decimal source, int factor)
+		{
+			return source * (1 + (decimal)factor / 100);
+		}
+
+	}
+}
