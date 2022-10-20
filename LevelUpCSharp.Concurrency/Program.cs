3443f685f4f87f46fd43861751dabe60a8bc0f9d
Pior Barankiewicz <piotr.barankiewicz@gmail.com>
2022-10-20 09:43:37 +0200

    synchronization - thread safe valt

--- LevelUpCSharp.Concurrency/Program.cs
+++ LevelUpCSharp.Concurrency/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace LevelUpCSharp.Concurrency
@@ -22,56 +23,28 @@ namespace LevelUpCSharp.Concurrency
 
         private static void WorkB(object? obj)
         {
-            var vault = (Vault<int>)obj;
+	        var vault = (Vault<int>) obj;
 
-            while (true)
-            {
-                var found = r.Next(100);
-
-				lock (vault)
-				{
-					Console.WriteLine("[B] i have: " + found);
-					vault.Put(found);
-					Console.WriteLine("[B] stored: " + found);
-
-					Console.WriteLine("[B] break");
-					Thread.Sleep(3 * 1000);
-					Console.WriteLine("[B] after break");
-
-
-					var get = vault.Get();
-					Console.WriteLine("[B] get:" + get); 
-				}
-
-                Thread.Sleep(3 * 1000);
-            }
+	        while (true)
+	        {
+		        var found = r.Next(100);
+		        
+		        Console.WriteLine("[B] i have: " + found);
+		        vault.Put(found);
+		        Console.WriteLine("[B] stored: " + found);
+	        }
         }
 
         private static void WorkA(object? obj)
         {
-            var vault = (Vault<int>)obj;
+	        var vault = (Vault<int>) obj;
 
-            while (true)
-            {
-                var found = r.Next(100);
-
-				lock (vault)
-				{
-					Console.WriteLine("[A] i have: " + found);
-					vault.Put(found);
-					Console.WriteLine("[A] stored: " + found);
-
-					Console.WriteLine("[A] break");
-					Thread.Sleep(7 * 1000);
-					Console.WriteLine("[A] after break");
-
-
-					var get = vault.Get();
-					Console.WriteLine("[A] get:" + get); 
-				}
-
-                Thread.Sleep(7 * 1000);
-            }
+	        while (true)
+	        {
+		        Console.WriteLine("[A] get attempt");
+		        var get = vault.Get();
+		        Console.WriteLine("[A] get:" + get);
+	        }
         }
     }
 }
--- LevelUpCSharp.Concurrency/Vault.cs
+++ LevelUpCSharp.Concurrency/Vault.cs
@@ -1,20 +1,32 @@
 ﻿using System;
+using System.Threading;
 
 namespace LevelUpCSharp.Concurrency
 {
     public class Vault<TSecret>
     {
         private TSecret _secret;
+        private SemaphoreSlim _in = new SemaphoreSlim(1);
+        private SemaphoreSlim _out = new SemaphoreSlim(0);
 
         public void Put(TSecret secret)
         {
-            _secret = secret;
+	        _in.Wait();
+
+	        _secret = secret;
+	        
+	        _out.Release();
         }
 
         public TSecret Get()
         {
+	        _out.Wait();
+
             var result = _secret;
             _secret = default(TSecret);
+            
+            _in.Release();
+
             return result;
         }
     }
